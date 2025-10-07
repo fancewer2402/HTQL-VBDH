bb64f9f83af75686e9b9f0ea64b6d6ebf7ef5028
Trinh-20405 <totrinh200405@example.com>
2025-10-07 08:41:32 +0700

    Tạo model vanbandi

--- VBDH/QLVB/models.py
+++ VBDH/QLVB/models.py
@@ -22,6 +22,22 @@ class NhanVien(models.Model):
 
     def __str__(self):
         return self.ho_ten
-from django.db import models
 
-# Create your models here.
+
+class VanBanDi(models.Model):
+    id = models.AutoField(primary_key=True)  # ID
+    so_hieu = models.CharField(max_length=50, unique=True)  # SoHieu
+    ngay_ban_hanh = models.DateField(null=True, blank=True)  # NgayBanHanh
+    trich_yeu = models.CharField(max_length=255)  # TrichYeu
+    noi_dung = models.TextField()  # NoiDung
+    loai_vb_di = models.CharField(max_length=255)  # LoaiVbDi
+    don_vi_nhan = models.CharField(max_length=255, null=True, blank=True)  # DonViNhan
+    file_dinh_kem = models.CharField(max_length=255, null=True, blank=True)  # DinhKem
+    do_mat = models.CharField(max_length=255, null=True, blank=True)  # DoMat
+    do_khan = models.CharField(max_length=255, null=True, blank=True)  # DoKhan
+    trang_thai = models.CharField(max_length=255, null=True, blank=True)  # TrangThai
+    ngay_tao = models.DateTimeField(auto_now_add=True)  # NgayTao
+    ma_nhan_vien = models.ForeignKey(NhanVien, on_delete=models.CASCADE)  # MaNhanVien
+
+    def __str__(self):
+        return self.trich_yeu
