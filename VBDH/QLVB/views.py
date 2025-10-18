4ef76267b84894ff9067e8c3faebe978d2d59236
Nguyen Thi Thu Thao <nguyenthaoo2609@gmail.com>
2025-10-18 23:47:10 +0700

    xem danh sach vb

--- VBDH/QLVB/views.py
+++ VBDH/QLVB/views.py
@@ -34,3 +34,44 @@ def logout_success(request):
     return render(request, 'QLVB/logout_success.html')
 def tra_cuu_van_ban(request):
     return render(request, 'QLVB/tra_cuu_van_ban.html')
+
+
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.db.models import Q
+from .models import VanBanDen, PhongBan, NhanVien
+from django.utils.dateparse import parse_date
+
+def get_current_nhanvien(request):
+    # Nếu bạn chưa map user -> NhanVien, trả về None (đổi logic nếu cần)
+    try:
+        email = request.user.email
+        return NhanVien.objects.filter(Email=email).first()
+    except Exception:
+        return None
+
+def danh_sach_van_ban_den(request):
+    van_ban_list = VanBanDen.objects.all().order_by('-NgayDen')
+    paginator = Paginator(van_ban_list, 10)  # mỗi trang 10 văn bản
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'QLVB/danh_sach_van_ban_den.html', {
+        'van_ban_list': page_obj.object_list,
+        'page_obj': page_obj,
+        'paginator': paginator,
+    })
+
+# def chi_tiet_van_ban_den(request, id):
+#     vb = get_object_or_404(VanBanDen, id=id)
+#     return render(request, 'QLVB/chi_tiet_vb_den.html', {'vb': vb})
+
+def chi_tiet_vb_den(request, vb_id):
+    vb = get_object_or_404(VanBanDen, id=vb_id)
+    vb_truoc = VanBanDen.objects.filter(id__lt=vb.id).order_by('-id').first()
+    vb_sau = VanBanDen.objects.filter(id__gt=vb.id).order_by('id').first()
+    return render(request, 'QLVB/chi_tiet_vb_den.html', {
+        'vb': vb,
+        'vb_truoc': vb_truoc,
+        'vb_sau': vb_sau
+    })
