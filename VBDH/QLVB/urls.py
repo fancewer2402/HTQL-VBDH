e03cd2e81de1876dd36d04b6079c079f7253f880
Trinh-20405 <totrinh200405@example.com>
2025-10-19 15:40:03 +0700

    vanbandi

--- VBDH/QLVB/urls.py
+++ VBDH/QLVB/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('tra-cuu-van-ban/', views.tra_cuu_van_ban, name='tra_cuu_van_ban'),
     path('logout/', views.logout_confirm, name='logout_confirm'),
     path('logout-success/', views.logout_success, name='logout_success'),
-    path('vanbandi/', views.vanbandi, name='vanbandi'),
+    path('vanbandi/', views.ds_vanbandi, name='vanbandi'),
+    path('vanbandi/<int:pk>/', views.vanbandi_detail, name='vanbandi_detail'),
 ]
