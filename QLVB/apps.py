45bed49356fcb4b2676ac40121dfa2dd8724281a
Trần Thị Thùy Trang <126124982+fancewer2402@users.noreply.github.com>
2025-11-22 22:58:50 +0700

    Revert "Feature/chuc nang ban hanh van ban di"

--- QLVB/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class QlvbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'QLVB'
-
-    def ready(self):
-        import QLVB.signals
